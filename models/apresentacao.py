10b365d117509adde1e8126e35423dac51131485
arvieira <andreribeirovieira@gmail.com>
2023-06-30 11:46:39 -0300

    Alteração do código da apresentação do segundo trabalho

--- models/apresentacao.py
+++ models/apresentacao.py
@@ -1,38 +1,38 @@
-import pandas as pd
 from xgboost import XGBClassifier
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.metrics import accuracy_score
 from sklearn import datasets
 
-# TODO Verificar se precisa disso tudo na carga do iris ou se posso usar iris['data'] e iris['target']
 
 # Carregando a base do Iris
 iris = datasets.load_iris()
 
-# Criando um DataFrame
-iris_df = pd.DataFrame(iris.data)
-iris_df['class'] = iris.target
-
-# Nomeando as colunas e removendo missing values
-iris_df.columns = ['sepal_len', 'sepal_wid', 'petal_len', 'petal_wid', 'class']
-iris_df.dropna(how="all", inplace=True)
-
-# Separando variáveis de entrada e saída
-iris_X = iris_df.iloc[:, [0, 1, 2, 3]]
-iris_Y = iris_df['class']
-
 # Dividindo a base em treino e teste
-X_train, X_test, y_train, y_test = train_test_split(iris_X, iris_Y, test_size=0.3, random_state=1234513)
-
-# TODO colocar um GridSearchCV para os hyperparâmetros do XGBoost
+X_train, X_test, y_train, y_test = train_test_split(iris.data, iris.target, test_size=0.3, random_state=1234513)
 
 # Criando o modelo com hyperparâmetros
-model = XGBClassifier(n_estimators=50, subsample=0.5, max_depth=4, eta=0.1, gamma=0, reg_alpha=0, reg_lambda=1,
-                      scale_pos_weight=0.2, booster='gbtree', eval_metric='logloss')
-model.fit(X_train, y_train)
-print(model)
+model = XGBClassifier()
+params = {
+    'n_estimators': [30, 50, 70],
+    'subsample': [0.5, 1],
+    'max_depth': [4, 6],
+    'eta': [0.1, 0.5, 1],
+    'booster': ['gbtree'],
+    'eval_metric': ['logloss']
+}
+grid = GridSearchCV(
+    estimator=model,
+    param_grid=params,
+    scoring='accuracy',
+    cv=5
+)
+grid.fit(X_train, y_train)
+print(grid.best_estimator_)
+print(grid.best_params_)
 
 # Imprimindo a acurácia
+model = grid.best_estimator_
 y_pred = model.predict(X_test)
 accuracy = accuracy_score(y_test, y_pred)
 print("Acurácia: %.2f%%" % (accuracy * 100.0))
+
